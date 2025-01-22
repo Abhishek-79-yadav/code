575b5c43982a5018e330c4f28d69f713bc75e787
Abhishek-79-yadav <abhishek7901yadav@gmail.com>
2025-01-22 22:38:18 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,10 @@
+public class swap {
+    public static void main(String[] args) {
+        int a=5,b=7,c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
